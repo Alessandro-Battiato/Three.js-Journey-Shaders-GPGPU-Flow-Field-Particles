33f601f51ba47f666ddc59ec52fe631c95dde278
alessandrobattiato <alessandroDev@outlook.it>
2024-06-01 19:20:52 +0200

    feat: particles rendered

--- src/script.js
+++ src/script.js
@@ -153,6 +153,30 @@ scene.add(gpgpu.debug); // this is just for debugging
  */
 const particles = {};
 
+// Geometry
+const particlesUvArray = new Float32Array(baseGeometry.count * 2);
+
+for (let y = 0; y < gpgpu.size; y++) {
+    for (let x = 0; x < gpgpu.size; x++) {
+        const i = y * gpgpu.size + x;
+        const i2 = i * 2;
+
+        // Particles UV
+        const uvX = (x + 0.5) / gpgpu.size;
+        const uvY = (y + 0.5) / gpgpu.size;
+
+        particlesUvArray[i2 + 0] = uvX;
+        particlesUvArray[i2 + 1] = uvY;
+    }
+}
+
+particles.geometry = new THREE.BufferGeometry();
+particles.geometry.setDrawRange(0, baseGeometry.count);
+particles.geometry.setAttribute(
+    "aParticlesUv",
+    new THREE.BufferAttribute(particlesUvArray, 2)
+);
+
 // Material
 particles.material = new THREE.ShaderMaterial({
     vertexShader: particlesVertexShader,
@@ -165,11 +189,12 @@ particles.material = new THREE.ShaderMaterial({
                 sizes.height * sizes.pixelRatio
             )
         ),
+        uParticlesTexture: new THREE.Uniform(),
     },
 });
 
 // Points
-particles.points = new THREE.Points(baseGeometry.instance, particles.material);
+particles.points = new THREE.Points(particles.geometry, particles.material);
 scene.add(particles.points);
 
 /**
@@ -200,6 +225,10 @@ const tick = () => {
 
     // GPGPU Update
     gpgpu.computation.compute();
+    particles.material.uniforms.uParticlesTexture.value =
+        gpgpu.computation.getCurrentRenderTarget(
+            gpgpu.particlesVariable
+        ).texture; // due to the ping pong buffer being used by the GPUComputationRenderer we use this to know which is the last FBO used for each frame
 
     // Render normal scene
     renderer.render(scene, camera);
