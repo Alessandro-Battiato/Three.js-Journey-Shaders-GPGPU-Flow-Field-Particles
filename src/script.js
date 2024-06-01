8928c2b88a9de22eaaa31861acb64b99f7d88d86
alessandrobattiato <alessandroDev@outlook.it>
2024-06-01 18:16:28 +0200

    feat: gpu computation render intialization in order to render FBO off screen

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { GPUComputationRenderer } from "three/examples/jsm/Addons.js";
 import GUI from "lil-gui";
 import particlesVertexShader from "./shaders/particles/vertex.glsl";
 import particlesFragmentShader from "./shaders/particles/fragment.glsl";
@@ -87,12 +88,31 @@ debugObject.clearColor = "#29191f";
 renderer.setClearColor(debugObject.clearColor);
 
 /**
+ * Base geometry
+ */
+const baseGeometry = {};
+baseGeometry.instance = new THREE.SphereGeometry(3);
+baseGeometry.count = baseGeometry.instance.attributes.position.count;
+
+/**
+ * GPU Compute
+ */
+// Setup
+const gpgpu = {};
+gpgpu.size = Math.ceil(Math.sqrt(baseGeometry.count)); // we need to round the number so we use Ceil because we have to create an FBO that even if it has leftover pixels it doesn't matter, each pixel needs it's own space
+gpgpu.computation = new GPUComputationRenderer(
+    gpgpu.size,
+    gpgpu.size,
+    renderer // provide renderer for off-page render
+);
+
+// Base particles
+const baseParticlesTexture = gpgpu.computation.createTexture();
+
+/**
  * Particles
  */
 const particles = {};
-
-// Geometry
-particles.geometry = new THREE.SphereGeometry(3);
 
 // Material
 particles.material = new THREE.ShaderMaterial({
@@ -110,7 +130,7 @@ particles.material = new THREE.ShaderMaterial({
 });
 
 // Points
-particles.points = new THREE.Points(particles.geometry, particles.material);
+particles.points = new THREE.Points(baseGeometry.instance, particles.material);
 scene.add(particles.points);
 
 /**
