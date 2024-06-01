bab6a10a4b1e9af6a7030e4cdd64f9ca7ab5c37e
alessandrobattiato <alessandroDev@outlook.it>
2024-06-01 19:47:30 +0200

    feat: implemented flow field with noise

--- src/shaders/gpgpu/particles.glsl
+++ src/shaders/gpgpu/particles.glsl
@@ -1,7 +1,18 @@
 // We are going to handle the flow field here
+#include ../includes/simplexNoise4d.glsl
+
 void main() {
     vec2 uv = gl_FragCoord.xy / resolution.xy;
     vec4 particle = texture(uParticles, uv); // we already have access to uParticles without importing the uniform
-    particle.y += 0.01;
+    
+    // Flow field
+    vec3 flowField = vec3(
+        simplexNoise4d(vec4(particle.xyz + 0.0, 0.0)), // x
+        simplexNoise4d(vec4(particle.xyz + 1.0, 0.0)), // y
+        simplexNoise4d(vec4(particle.xyz + 2.0, 0.0)) // z
+    ); // the direction towards which the particles should move
+    flowField = normalize(flowField); // being a direction, directions need to be normalized
+    particle.xyz += flowField * 0.01; // we "nerf" the flow field by multiplying it
+
     gl_FragColor = particle;
 }
